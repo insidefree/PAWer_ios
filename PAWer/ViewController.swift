4303b8344431bbdb90ce58b4778fd2814991d5d3
Denis Sorokin <denys.sorokin@gmail.com>
2017-11-01 22:12:30 +0200

    Added outlets and actions for LoginViewController

--- PAWer/ViewController.swift
+++ PAWer/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var pswdTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,5 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func loginTapped(_ sender: Any) {
+        print("login")
+    }
+    @IBAction func registerTapped(_ sender: Any) {
+        print("register")
+    }
 }
 
