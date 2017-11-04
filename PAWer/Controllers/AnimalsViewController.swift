ce0ce0c7995fa7f644a3d574be6203f02ec4abc8
Denis Sorokin <denys.sorokin@gmail.com>
2017-11-04 15:46:57 +0200

    Added AddAnimalAlert

--- PAWer/Controllers/AnimalsViewController.swift
+++ PAWer/Controllers/AnimalsViewController.swift
@@ -25,8 +25,18 @@ class AnimalsViewController: UIViewController, UITableViewDelegate, UITableViewD
         return 10
     }
     @IBAction func addTapped(_ sender: UIBarButtonItem) {
-        print("add")
+        let alertController = UIAlertController(title: "New animal", message: "Add new animal", preferredStyle: .alert)
+        alertController.addTextField()
+        let save = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let textField = alertController.textFields?.first, textField.text != "" else {return}
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alertController.addAction(save)
+        alertController.addAction(cancel)
+        
+        present(alertController, animated: true, completion: nil)
     }
+    
     @IBAction func signOutTapped(_ sender: UIBarButtonItem) {
         do {
             try Auth.auth().signOut()
