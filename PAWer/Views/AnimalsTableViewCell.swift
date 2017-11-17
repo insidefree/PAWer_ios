3f22e5bce5c4f6714d890b0348bb5fca8d128562
Denis Sorokin <denys.sorokin@gmail.com>
2017-11-17 19:10:26 +0200

    Added first navigation screen

---
+++ PAWer/Views/AnimalsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnimalsTableViewCell.swift
+//  PAWer
+//
+//  Created by Denis Sorokin on 17/11/2017.
+//  Copyright © 2017 Denis Sorokin. All rights reserved.
+//
+
+import UIKit
+
+class AnimalsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
